3b8ea00e7121e743ff99f9aff320dca92ef9cf5d
Igor <igor.koretskiy1@gmail.com>
2023-05-23 21:18:23 +0100

    Started creating Core. Added a PasswordHasher class in Helpers.

---
+++ BLL/Core/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public Duty duty{ get; set; }
+        public bool IsBuisy { get; set; }
+    }
+}
---
+++ BLL/Helpers/PasswordHasher.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography;
+using System;
+
+namespace Helpers
+{
+    public static class PasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 20;
+        private const int Iterations = 10000;
+        private static readonly byte[] _salt;
+
+        static PasswordHasher()
+        {
+            _salt = new byte[SaltSize];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(_salt);
+            }
+        }
+
+        public static string HashPassword(string password)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, _salt, Iterations))
+            {
+                var hash = pbkdf2.GetBytes(HashSize);
+                var hashBytes = new byte[SaltSize + HashSize];
+                Array.Copy(_salt, 0, hashBytes, 0, SaltSize);
+                Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
+            var salt = new byte[SaltSize];
+            Array.Copy(hashedPasswordBytes, 0, salt, 0, SaltSize);
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations))
+            {
+                var hash = pbkdf2.GetBytes(HashSize);
+                for (int i = 0; i < HashSize; i++)
+                {
+                    if (hashedPasswordBytes[i + SaltSize] != hash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
